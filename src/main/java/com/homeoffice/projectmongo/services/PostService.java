9bc4d400b46d5367c13b1069e549b76346b51e32
thaleswillreis <thalewill@yahoo.com.br>
2022-06-07 14:59:29 -0300

    Obtendo um post pelo id

---
+++ src/main/java/com/homeoffice/projectmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.homeoffice.projectmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.homeoffice.projectmongo.domain.Post;
+import com.homeoffice.projectmongo.repository.PostRepository;
+import com.homeoffice.projectmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+/*	Obs: Codigo comentado serve apenas para o versoes do Spring Boot abaixo do 2.x
+	public Post findById(String id) {
+		Post user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+*/	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}	
+}
