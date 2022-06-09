2cac88f10fb9c7dac090dd61eb55e83e107e8100
thaleswillreis <thalewill@yahoo.com.br>
2022-06-08 23:56:53 -0300

    Consulta simples com @Query

--- src/main/java/com/homeoffice/projectmongo/repository/PostRepository.java
+++ src/main/java/com/homeoffice/projectmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.homeoffice.projectmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.homeoffice.projectmongo.domain.Post;
 
 @Repository		//camada repository acessa o BD
 public interface PostRepository extends MongoRepository<Post, String> { 
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") //'?0' indica o parametro a ser capturado, nesse caso o (String text), 'i' indica Case insensitivity
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
